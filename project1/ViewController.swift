6d837e84b9d68b1ee534ced32cdf84c508bacd3f
alexsahnykov <38592964+alexsahnykov@users.noreply.github.com>
2018-06-17 17:11:24 +0300

    Create tableview

--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -8,9 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController,  UITableViewDelegate , UITableViewDataSource   {
+    
+    var programmingLanguage = ["Swift","JS","Ruby","Rython","C++","C#","PHP","Java","Scala","Pascal"]
+    
+    var programmingImages = ["Swift.jpg","JS.jpg","Ruby.jpg","Rython.jpg","C++.jpg","C#.jpg","PHP.jpg","Java.jpg","Scala.jpg","Javav.jpg"]
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return programmingLanguage.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = programmingLanguage [indexPath.row]
+       cell?.imageView?.image = UIImage (named: programmingLanguage [indexPath.row] )
+        return cell!
+    }
+   
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) { // Delete tableview cell
+        if editingStyle == .delete {
+            self.programmingLanguage.remove(at: indexPath.row)
+            self.programmingImages.remove(at: indexPath.row)
+        }
+        tableView.deleteRows(at: [indexPath], with: .fade)
+}
+    
+    
+     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -22,4 +47,3 @@ class ViewController: UIViewController {
 
 
 }
-
